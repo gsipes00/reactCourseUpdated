37ee7f19dbd3adec44976810524a25a81463a77a
Gene Sipes <gsipes00@yahoo.com>
2022-12-13 21:29:07 -0600

    add comments to code and created clear list function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -24,6 +24,7 @@ function App() {
       // deal with edit
     } else {
       // show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -34,6 +35,12 @@ function App() {
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
   };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -54,7 +61,9 @@ function App() {
       {list.length > 0 && (
         <div className='grocery-container'>
           <List items={list} />
-          <button className='clear-btn'>clear items</button>
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
