8ff894b24e62c83548483e2fd00eb73ce731f210
Gene Sipes <gsipes00@yahoo.com>
2023-01-28 13:06:14 -0600

    set up useContext hook within context.js, created custom hook useGlobalContext to make passing in AppProvider and useContext easier for each child component.

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,16 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext("bye  bye");
+console.log(AppContext);
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+};
+
+// custom hook for global import of useContext and AppContext
+// allows for easier import between descendant components
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- 12-sidebar-modal/setup/src/index.js
+++ 12-sidebar-modal/setup/src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { AppProvider } from './context';
+import { AppProvider } from "./context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// the entire App Component, which has children components <Home/>, <Modal/>, and <Sidebar/>
+// gets wrapped in the Provider Component, and gives access to the Value property.
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
