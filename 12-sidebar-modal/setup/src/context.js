4d9dc0882f7db145c2a2b59173e008d4dc75061e
Gene Sipes <gsipes00@yahoo.com>
2023-01-30 18:59:01 -0600

    created open/close sidebar/modal functions, added the function and state variables to the app provider as props, imported the app provider to modal.js and sidebar.js with the global useContext function, added the close/open modal and sidebar functions as onClick functions to the proper nodes.

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,14 +1,46 @@
 import React, { useState, useContext } from "react";
 
-const AppContext = React.createContext("bye  bye");
-console.log(AppContext);
+// creates a Context object, pass in default value if needed
+const AppContext = React.createContext();
 
+// Context object has a Provider Component. Pass in Value to the component and add children component object
+// Any children of the Provider Component will have access to the value passed in
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // open/close sidebar and modal functions
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // custom hook for global import of useContext and AppContext
 // allows for easier import between descendant components
+// must use the prefix "use" for custom hook
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
