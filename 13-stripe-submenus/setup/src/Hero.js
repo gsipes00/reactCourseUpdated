771c2e2092dbaf69075d2461e9a23d0f1b29a885
Gene Sipes <gsipes00@yahoo.com>
2023-01-30 20:55:13 -0600

    setup hero.js with useContext import, created JSX for hero

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -2,9 +2,29 @@ import React from "react";
 import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
   const data = useGlobalContext();
   console.log(data);
-  return <h2>hero component</h2>;
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
