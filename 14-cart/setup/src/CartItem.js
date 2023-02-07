cb250ec7cb087c815370577af87405852f522a0e
Gene Sipes <gsipes00@yahoo.com>
2023-02-06 21:21:05 -0600

    testing out functionality by adding an additional property to data.js objects, name. Referenced name in the CartItems.js jsx next to the titled

--- 14-cart/setup/src/CartItem.js
+++ 14-cart/setup/src/CartItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useGlobalContext } from "./context";
-const CartItem = ({ id, img, title, price, amount }) => {
+
+const CartItem = ({ id, img, title, price, amount, name }) => {
   const { remove } = useGlobalContext();
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
       <div>
-        <h4>{title}</h4>
+        <h4>
+          {title} (sold by {name})
+        </h4>
         <h4 className='item-price'>${price}</h4>
         {/* remove button */}
         <button className='remove-btn' onClick={() => remove(id)}>
