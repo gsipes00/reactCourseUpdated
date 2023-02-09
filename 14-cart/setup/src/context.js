277c1a7770a76f7795df7b2a02cc57f87feae419
Gene Sipes <gsipes00@yahoo.com>
2023-02-08 21:01:38 -0600

    created increase and decrease functions, passing in dispatch to each. Added increase and decrease to the click functions in cartItem.js, setup functionality for each function in reduce.js

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -1,8 +1,7 @@
 import React, { useState, useContext, useReducer, useEffect } from "react";
 import cartItems from "./data";
 import reducer from "./reducer";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
 
@@ -12,26 +11,38 @@ const initialState = {
   cart: cartItems,
   total: 0,
   amount: 0,
+  quantity: 0,
   name: ["Gene", "Charles", "Robby", "Thurston"],
 };
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  // clear cart function
+  // clear cart function in CartContainer.js
   const clearCart = () => {
     dispatch({ type: "CLEAR_CART" });
   };
-  // remove single item
+  // remove single item function in CartItems.js
   const remove = (id) => {
     dispatch({ type: "REMOVE", payload: id });
   };
+  // increase quantitiy
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  // decrease quantitiy
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
         remove,
+        increase,
+        decrease,
       }}
     >
       {children}
