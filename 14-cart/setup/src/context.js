92015976e03af9071a6536fe185ba5dbbe8ebad2
Gene Sipes <gsipes00@yahoo.com>
2023-02-13 20:17:47 -0600

    final task set up api fetch for data, instead of using data.js

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -33,6 +33,18 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: "DECREASE", payload: id });
   };
+  // fetch
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   // useEffect
   useEffect(() => {
     dispatch({ type: "GET_TOTALS" });
